39fb565d0774ceba07b18ea73863320a07a1a2c8
ilnursoft <ilnursoft@gmail.com>
2022-05-24 21:40:06 +0300

    add пустой файл тестов

---
+++ test_tatar_translit.py
@@ -0,0 +1,15 @@
+import unittest
+
+from tatar_translit import get_alifbasi
+
+
+class TestUser(unittest.TestCase):
+    """
+        тесты
+    """
+
+    def setUp(self) -> None:
+        pass
+
+    def test_get_alifbasi(self):
+        pass
